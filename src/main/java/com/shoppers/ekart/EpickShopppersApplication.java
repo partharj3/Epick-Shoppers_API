996f9da8b008156004e86fae91ee9b8948cf4928
partharj3 <ramnagsarathy@gmail.com>
2024-02-13 08:18:04 +0530

    initialized e-commerce application

---
+++ src/main/java/com/shoppers/ekart/EpickShopppersApplication.java
@@ -0,0 +1,13 @@
+package com.shoppers.ekart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpickShopppersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpickShopppersApplication.class, args);
+	}
+
+}
